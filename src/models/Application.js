9078085c3a210f9131430bbc8bfa70816178eb9f
Somesh Kumar <kumarsomesh.unos@gmail.com>
2023-01-18 12:42:22 +0530

    add new images

--- src/models/Application.js
+++ src/models/Application.js
@@ -19,7 +19,7 @@ const ApplicantDetailsSchema = new mongoose.Schema({
   },
   gender: {
     type: String,
-    enum: ["Male", "Female"],
+    enum: ["male", "female"],
     required: [true, "Gender is required"],
   },
   religion: {
